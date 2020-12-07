7e583fb1aea94edb72d73fed01a67298660e3669
tchan90 <47408197+tchan90@users.noreply.github.com>
2020-12-07 22:21:54 +1100

    reduce mock data

--- src/pages/About.test.js
+++ src/pages/About.test.js
@@ -8,37 +8,13 @@ afterEach(cleanup);
 
 const mockAboutData = [
 	{
-		acf: [],
 		author: 1,
-		comment_status: "closed",
 		content: {
 			protected: false,
 			rendered: "",
 		},
-		date: "2020-04-11T12:00:50",
-		date_gmt: "2020-04-11T12:00:50",
-		excerpt: {
-			protected: false,
-			rendered: "",
-		},
-		featured_media: 0,
-		guid: {
-			rendered: "",
-		},
 		id: 1,
-		link: "",
-		menu_order: 0,
-		meta: [],
-		modified: "2020-04-11T12:00:50",
-		modified_gmt: "2020-04-11T12:00:50",
-		parent: 0,
-		ping_status: "closed",
-		slug: "about",
-		status: "publish",
-		template: "",
-		title: { rendered: "About-Test" },
-		type: "page",
-		yoast_head: "",
+		title: { rendered: "About Test Page" },
 	},
 ];
 
