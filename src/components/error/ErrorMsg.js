d4d9faa1b803bdb2b942fb4de11b299046cd4768
tchan90 <47408197+tchan90@users.noreply.github.com>
2021-06-20 15:41:19 +1000

    instagram updates

--- src/components/error/ErrorMsg.js
+++ src/components/error/ErrorMsg.js
@@ -4,7 +4,10 @@ export default function ErrorMsg() {
 	return (
 		<div className="text-center error-msg">
 			<p> Woops something went wrong!</p>
-			<p>Please contact me if this message persists</p>
+			<p>
+				If this message persists try to empty your cache and reload the page.
+				Otherwise feel free to contact me!
+			</p>
 			<img
 				className="vector-icons"
 				src="https://www.delightfullytastymelb.com/wp-content/uploads/2020/06/kingdom-404-error.png"
